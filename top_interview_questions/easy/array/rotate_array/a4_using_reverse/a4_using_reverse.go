cc5ec6c607088b9dc5118a380f8ca773bd1aff4e
Lillard Kuo <lillard.kuo@lileesystems.com>
2023-12-16 18:24:00 +0800

    Add the approach 4 using reverse to solve the rotate problem.

---
+++ top_interview_questions/easy/array/rotate_array/a4_using_reverse/a4_using_reverse.go
@@ -0,0 +1,17 @@
+package a4_using_reverse
+
+func rotate(nums []int, k int) {
+	n := len(nums)
+	k = k % n
+	reverse(nums)
+	reverse(nums[0:k])
+	reverse(nums[k:n])
+}
+
+func reverse(nums []int) {
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		temp := nums[i]
+		nums[i] = nums[j]
+		nums[j] = temp
+	}
+}
---
+++ top_interview_questions/easy/array/rotate_array/a4_using_reverse/a4_using_reverse_test.go
@@ -0,0 +1,59 @@
+package a4_using_reverse
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRotateWhenOneItems(t *testing.T) {
+	// Arrange
+	nums := []int{1}
+	k := 3
+
+	// Act
+	rotate(nums, k)
+
+	// Assert
+	expectedNums := []int{1}
+	assert.Equal(t, expectedNums, nums)
+}
+
+func TestRotateWhenKLessThanLengthOfNums(t *testing.T) {
+	// Arrange
+	nums := []int{1, 2, 3, 4, 5}
+	k := 3
+
+	// Act
+	rotate(nums, k)
+
+	// Assert
+	expectedNums := []int{3, 4, 5, 1, 2}
+	assert.Equal(t, expectedNums, nums)
+}
+
+func TestRotateWhenKEqualToLengthOfNums(t *testing.T) {
+	// Arrange
+	nums := []int{1, 2, 3, 4, 5}
+	k := 5
+
+	// Act
+	rotate(nums, k)
+
+	// Assert
+	expectedNums := []int{1, 2, 3, 4, 5}
+	assert.Equal(t, expectedNums, nums)
+}
+
+func TestRotateWhenKLargerThanLengthOfNums(t *testing.T) {
+	// Arrange
+	nums := []int{1, 2, 3, 4, 5}
+	k := 6
+
+	// Act
+	rotate(nums, k)
+
+	// Assert
+	expectedNums := []int{5, 1, 2, 3, 4}
+	assert.Equal(t, expectedNums, nums)
+}
