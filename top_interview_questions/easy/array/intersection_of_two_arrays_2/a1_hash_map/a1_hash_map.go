e4cb631b9c7b6dada1e7390f9d8fc20f3d32d5a8
Lillard Kuo <lillard.kuo@lileesystems.com>
2023-12-20 01:25:59 +0800

    Finish the approach 2, hash map, for the problem, intersection of two arrays 2.

---
+++ top_interview_questions/easy/array/intersection_of_two_arrays_2/a1_hash_map/a1_hash_map.go
@@ -0,0 +1,18 @@
+package a1_hash_map
+
+func intersect(nums1 []int, nums2 []int) []int {
+	nums1Map := make(map[int]int)
+
+	for _, n := range nums1 {
+		nums1Map[n] += 1
+	}
+
+	result := []int{}
+	for _, n := range nums2 {
+		if v, ok := nums1Map[n]; ok && v != 0 {
+			result = append(result, n)
+			nums1Map[n] -= 1
+		}
+	}
+	return result
+}
---
+++ top_interview_questions/easy/array/intersection_of_two_arrays_2/a2_sort/a2_sort.go
@@ -0,0 +1,6 @@
+package a2_sort
+
+func intersect(nums1 []int, nums2 []int) []int {
+	// TODO: a2_sort
+	return nil
+}
