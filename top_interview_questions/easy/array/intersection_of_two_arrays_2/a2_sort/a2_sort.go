7f27d75104f3bcfb023d4fd1855a2d3e76f7fb87
Lillard Kuo <lillard.kuo@lileesystems.com>
2023-12-21 10:50:41 +0800

    Add the approach 2, using sort for the intersection of two arrays problem.

--- top_interview_questions/easy/array/intersection_of_two_arrays_2/a2_sort/a2_sort.go
+++ top_interview_questions/easy/array/intersection_of_two_arrays_2/a2_sort/a2_sort.go
@@ -1,6 +1,30 @@
 package a2_sort
 
+import "sort"
+
 func intersect(nums1 []int, nums2 []int) []int {
-	// TODO: a2_sort
-	return nil
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+
+	i, j := 0, 0
+	intersection := []int{}
+
+	for {
+		if i >= len(nums1) || j >= len(nums2) {
+			break
+		}
+
+		if nums1[i] < nums2[j] {
+			i++
+		} else if nums1[i] > nums2[j] {
+			j++
+		} else {
+			intersection = append(intersection, nums1[i])
+			i++
+			j++
+		}
+
+	}
+
+	return intersection
 }
--- top_interview_questions/easy/array/intersection_of_two_arrays_2/a2_sort/a2_sort_test.go
+++ top_interview_questions/easy/array/intersection_of_two_arrays_2/a2_sort/a2_sort_test.go
@@ -1,1 +1,85 @@
 package a2_sort
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWhenNums1EqualsToNums2AndLengthIsOne(t *testing.T) {
+	// Arrange
+	nums1 := []int{1}
+	nums2 := []int{1}
+
+	// Act
+	result := intersect(nums1, nums2)
+
+	// Assert
+	expectedIntersectionNums := []int{1}
+	assert.Equal(t, expectedIntersectionNums, result)
+}
+
+func TestWhenNums1EqualsToNums2AndLengthNotOne(t *testing.T) {
+	// Arrange
+	nums1 := []int{1, 2}
+	nums2 := []int{1, 2}
+
+	// Act
+	result := intersect(nums1, nums2)
+
+	// Assert
+	expectedIntersectionNums := []int{1, 2}
+	assert.Equal(t, expectedIntersectionNums, result)
+}
+
+func TestWhenIntersectionNumCountGreaterThenOne(t *testing.T) {
+	// Arrange
+	nums1 := []int{1, 4, 4}
+	nums2 := []int{1, 4, 4}
+
+	// Act
+	result := intersect(nums1, nums2)
+
+	// Assert
+	expectedIntersectionNums := []int{1, 4, 4}
+	assert.Equal(t, expectedIntersectionNums, result)
+}
+
+func TestWhenNums1IncludsNums2(t *testing.T) {
+	// Arrange
+	nums1 := []int{1, 4, 4, 5, 6}
+	nums2 := []int{1, 4, 4}
+
+	// Act
+	result := intersect(nums1, nums2)
+
+	// Assert
+	expectedIntersectionNums := []int{1, 4, 4}
+	assert.Equal(t, expectedIntersectionNums, result)
+}
+
+func TestWhenNums1IntersectsWithNums2(t *testing.T) {
+	// Arrange
+	nums1 := []int{1, 2, 2, 4}
+	nums2 := []int{1, 4}
+
+	// Act
+	result := intersect(nums1, nums2)
+
+	// Assert
+	expectedIntersectionNums := []int{1, 4}
+	assert.Equal(t, expectedIntersectionNums, result)
+}
+
+func TestWhenNums1AndNums2AreDisjoint(t *testing.T) {
+	// Arrange
+	nums1 := []int{1, 2, 2, 4}
+	nums2 := []int{8, 8, 9}
+
+	// Act
+	result := intersect(nums1, nums2)
+
+	// Assert
+	expectedIntersectionNums := []int{}
+	assert.Equal(t, expectedIntersectionNums, result)
+}
