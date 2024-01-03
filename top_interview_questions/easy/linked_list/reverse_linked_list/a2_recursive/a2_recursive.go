1a6bfbcd2210e959249fdcd4c9750f4c0f8c5c32
Lillard Kuo <lillard.kuo@lileesystems.com>
2024-01-03 21:02:01 +0800

    Add the approach 2, recursive, for the reverse linked list problem.

---
+++ top_interview_questions/easy/linked_list/reverse_linked_list/a2_recursive/a2_recursive.go
@@ -0,0 +1,19 @@
+package a2_recursive
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	// The newHead would be the last node
+	newHead := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+
+	return newHead
+}
