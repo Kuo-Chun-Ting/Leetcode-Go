56494ccd8a5706643abc14ac1b341a6a4a8c574f
Lillard Kuo <lillard.kuo@lileesystems.com>
2024-03-17 13:37:03 +0800

    Add the binets method of the climb stairs problem.

--- top_interview_questions/easy/dynamic_programming/climbing_stairs/a5_binets_method/a5_binets_method.go
+++ top_interview_questions/easy/dynamic_programming/climbing_stairs/a5_binets_method/a5_binets_method.go
@@ -1,6 +1,39 @@
 package a5_binets_method
 
 func climbStairs(n int) int {
-	// TODO: a5_binets_method
-	return 0
+	q := [][]int{{1, 1}, {1, 0}}
+	qN := power(q, n)
+	return qN[0][0]
+}
+
+func power(matrix [][]int, pow int) [][]int {
+	if pow == 0 {
+		return [][]int{{1, 0}, {0, 1}}
+	}
+
+	if pow == 1 {
+		return matrix
+	}
+
+	currentPower := 1
+	powerOfN := matrix
+
+	for {
+		powerOfN = multiply(powerOfN, matrix)
+		currentPower++
+
+		if currentPower == pow {
+			break
+		}
+	}
+	return powerOfN
+}
+
+func multiply(matrix1 [][]int, matrix2 [][]int) [][]int {
+	res := [][]int{{0, 0}, {0, 0}}
+	res[0][0] = matrix1[0][0]*matrix2[0][0] + matrix1[0][1]*matrix2[1][0]
+	res[0][1] = matrix1[0][0]*matrix2[0][1] + matrix1[0][1]*matrix2[1][1]
+	res[1][0] = matrix1[1][0]*matrix2[0][0] + matrix1[1][1]*matrix2[1][0]
+	res[1][1] = matrix1[1][0]*matrix2[0][1] + matrix1[1][1]*matrix2[1][1]
+	return res
 }
