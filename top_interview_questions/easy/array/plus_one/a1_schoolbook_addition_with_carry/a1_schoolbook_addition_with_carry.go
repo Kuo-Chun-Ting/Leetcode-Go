3706c178ce4f4812f495432d2cc1cd14c8a15d8b
Lillard Kuo <lillard.kuo@lileesystems>
2025-02-17 21:47:59 +0800

    Modify the plus one problem.

--- top_interview_questions/easy/array/plus_one/a1_schoolbook_addition_with_carry/a1_schoolbook_addition_with_carry.go
+++ top_interview_questions/easy/array/plus_one/a1_schoolbook_addition_with_carry/a1_schoolbook_addition_with_carry.go
@@ -1,15 +1,14 @@
 package a1_schoolbook_addition_with_carry
 
+// Check twice
 func plusOne(digits []int) []int {
 	for i := len(digits) - 1; i >= 0; i-- {
-		if digits[i] == 9 {
-			digits[i] = 0
-		} else {
+		if digits[i]+1 <= 9 {
 			digits[i] += 1
 			return digits
 		}
+		digits[i] = 0
 	}
 
-	digits = append([]int{1}, digits...)
-	return digits
+	return append([]int{1}, digits...)
 }
